62cedb676dd5e43e918a546ec5a52a79732e2716
subhiyyah <78728221+subhiyyah@users.noreply.github.com>
2026-08-16 20:17:27 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -126,5 +126,39 @@ public class Main {
 //        System.out.println("Array after putting odd number first  "+oddFirst);
         //ex6
         //write a program that test the equality of two arrays
+        System.out.println("enter the size of array 1 and two");
+        int siz6=input.nextInt();
+        ArrayList<Integer> arra1=new ArrayList<>();
+        ArrayList<Integer> arra2=new ArrayList<>();
+        int val6;
+        //تعبئة كل المصفوفتين
+        for(int i =0;i<siz6;i++){
+            System.out.println("enter the number in arra1 item  "+(i+1));
+            val6=input.nextInt();
+            arra1.add(i,val6);
+        }
+        for(int j =0;j<siz6;j++){
+
+            System.out.println("enter the number in arra2 item  "+(j+1));
+            val6=input.nextInt();
+            arra2.add(j,val6);
+        }
+        int equal=0;
+        for(int k = 0 ;k<siz6;k++){
+            if(arra1.get(k)==arra2.get(k)){
+                equal++;
+
+            }
+        }
+        if(equal==siz6){
+            System.out.println("this array are equal");
+
+        }else
+            System.out.println("this array are not equal"+"\n the score of equality is "+equal);
+
+
+        System.out.println(arra1);
+        System.out.println(arra2);
+
     }
 }
